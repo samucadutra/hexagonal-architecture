f7432103680735c6ce73ae114ceac94d72a98d7f
Samuel Dutra <samuel.dutra@hotmail.com>
2024-04-11 18:39:07 -0300

    AULA-9: criando o adapter de inserção do cliente

---
+++ src/main/java/com/dutra/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,11 @@
+package com.dutra.hexagonal.adapters.out.repository.mapper;
+
+import com.dutra.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.dutra.hexagonal.application.core.domain.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+}
