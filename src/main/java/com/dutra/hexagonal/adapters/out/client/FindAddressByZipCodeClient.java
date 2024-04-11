393eab63d30bb7b0aedf2d3c84fe6219e836e333
Samuel Dutra <samuel.dutra@hotmail.com>
2024-04-11 18:16:36 -0300

    AULA-6: criando o client

---
+++ src/main/java/com/dutra/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,16 @@
+package com.dutra.hexagonal.adapters.out.client;
+
+import com.dutra.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url = "${dutra.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+}
